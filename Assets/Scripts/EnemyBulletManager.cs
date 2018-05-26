1952c05dfd44ef3841c8c6a54f6a2983f591630c
Ryota Goto <pandoraxssscan.929@gmail.com>
2018-05-26 12:39:59 +0900

    敵の攻撃パターンに円形弾を追加

--- Assets/Scripts/EnemyBulletManager.cs
+++ Assets/Scripts/EnemyBulletManager.cs
@@ -10,7 +10,7 @@ public class EnemyBulletManager : MonoBehaviour
     /// <summary>
     /// 敵の弾の種類
     /// </summary>
-    public enum BulletType
+    public enum BulletColor
     {
         Red = 0,
         Blue,
@@ -28,6 +28,7 @@ public class EnemyBulletManager : MonoBehaviour
     [SerializeField]
     GameObject blueBulletPref;
 
+
     // ----------------------------------------------------------------------------------------------------------
     // public関数
     // ----------------------------------------------------------------------------------------------------------
@@ -35,22 +36,51 @@ public class EnemyBulletManager : MonoBehaviour
     /// <summary>
     /// targetに向けて弾を発射
     /// </summary>
-    /// <param name="bt">発射したい弾の色</param>
+    /// <param name="bc">発射したい弾の色</param>
     /// <param name="target">ターゲット</param>
-    public void aimBullet(BulletType bt, Vector3 target)
+    public void aimShot(BulletColor bc, Vector3 target)
     {
         var direction =  target - transform.position;
-        GameObject obj = null;
-        switch (bt) {
-            case BulletType.Red:
-                obj = Instantiate(redBulletPref);
-                break;
-            case BulletType.Blue:
-                obj = Instantiate(redBulletPref);
-                break;
-        }
-
+        GameObject obj = createBullet(bc);
+        
         // 弾の方向を渡す
-        obj.GetComponent<EnemyBullet>().initBullet(direction);
+        obj.GetComponent<EnemyBullet>().initBullet(direction.normalized);
+    }
+
+    /// <summary>
+    /// 円形に弾を発射
+    /// </summary>
+    /// <param name="bc">弾の色</param>
+    /// <param name="partitions">分割数</param>
+    public void circleShot(BulletColor bc, int partitions = 18)
+    {
+        for (int i = 0; i < partitions; ++i) {
+            float rot = 360.0f / partitions * i;
+            float x = Mathf.Cos(rot * Mathf.PI / 180.0f);
+            float y = Mathf.Sin(rot * Mathf.PI / 180.0f);
+            var obj = createBullet(bc);
+            obj.GetComponent<EnemyBullet>().initBullet(new Vector2(x, y).normalized);
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------------------
+    // private関数
+    // ----------------------------------------------------------------------------------------------------------
+
+    /// <summary>
+    /// 引数で指定された色の弾を生成
+    /// </summary>
+    /// <param name="bc">弾の色</param>
+    /// <returns>生成された弾</returns>
+    GameObject createBullet(BulletColor bc)
+    {
+        switch (bc) {
+            case BulletColor.Red:
+                return Instantiate(redBulletPref);
+            case BulletColor.Blue:
+                return Instantiate(blueBulletPref);
+            default:
+                return null;
+        }
     }
 }
