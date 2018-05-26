5ca62221790154e5de63cb41fc5ad5d901e576cc
Yuya Takeda <blocks.club@gmail.com>
2018-05-26 15:43:44 +0900

    imlement enemy AI 4ways

---
+++ Assets/Scripts/BossEnemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnemy : MonoBehaviour {
+
+	EnemyPatterns patterns;
+	public int rnd_max = 3;
+	public int rnd_min = 0;
+	public int state = 0;
+	public float diff = 0.3f;
+
+	// Use this for initialization
+	void Start () {
+		patterns = GetComponent<EnemyPatterns>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		patterns.movePatterns(state);
+		if (patterns.target.x-diff < transform.position.x && patterns.target.y-diff < transform.position.y
+			&& patterns.target.x+diff > transform.position.x && patterns.target.y+diff > transform.position.y){
+			
+			state = Random.Range(0,3);			
+		}
+			
+	}
+}
---
+++ Assets/Scripts/EnemyPatterns.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyPatterns : MonoBehaviour {
+
+	public Vector2 target = new Vector2(0,0);	
+	public Vector2 v = new Vector2();
+	public float time = 1.0f;
+	public Vector2 velocity;
+
+	// Use this for initialization
+	void Start () {
+//		superenemy = GetComponent<superEnemy>();
+	}
+	
+	public void movePatterns(int state){
+		// move right-up
+		if(state == 0){
+			target.x = 3;
+			target.y = 3;
+			transform.position = Vector2.SmoothDamp(transform.position,target,ref velocity,time,100,Time.deltaTime);
+		}
+		// move left-up
+		if(state == 1){
+			target.x = -3;
+			target.y = 3;
+			transform.position = Vector2.SmoothDamp(transform.position,target,ref velocity,time,100,Time.deltaTime);
+		}
+		// move left-down
+		if(state == 2){
+			target.x = -3;
+			target.y = -3;
+			transform.position = Vector2.SmoothDamp(transform.position,target,ref velocity,time,100,Time.deltaTime);
+		}
+		// move right-down
+		if(state == 3){
+			target.x = 3;
+			target.y = -3;
+			transform.position = Vector2.SmoothDamp(transform.position,target,ref velocity,time,100,Time.deltaTime);
+		}
+	}
+}
