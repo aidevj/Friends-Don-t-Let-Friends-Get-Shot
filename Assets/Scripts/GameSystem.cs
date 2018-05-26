385b7922ae2f85bddeb4894091ab17fd96cfffc9
Kaichi <kai0729s@yahoo.co.jp>
2018-05-26 16:50:22 +0900

    Scene作成

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameSystem : MonoBehaviour {
+
+    // Use this for initialization
+    public void GameStart() {
+        SceneManager.LoadScene("MainGame");
+
+    }
+
+    public void GameEnd()
+    {
+        Application.Quit();
+
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+}
---
+++ Assets/Scripts/ReturnToTitle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToTitle : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void ReturnTitle()
+    {
+        SceneManager.LoadScene("Title");
+    }
+}
