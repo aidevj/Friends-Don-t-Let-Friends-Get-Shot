1875c4148346da5c634ba173e9e2140a49dcfe2a
Yuya Takeda <blocks.club@gmail.com>
2018-05-26 18:08:13 +0900

    Bossに弾を撃たせた（仮）

--- Assets/Scripts/BossEnemy.cs
+++ Assets/Scripts/BossEnemy.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Bossを制御するクラス
+/// </summary>
 public class BossEnemy : MonoBehaviour {
 
 	EnemyPatterns patterns;
+	EnemyBulletManager EBM;
 	public int rnd_max = 3;
 	public int rnd_min = 0;
 	public int state = 0;
 	public float diff = 0.3f;
 	public int shotCtrl = 0;
 	
-	public int enemyLife = 2;
+	public int LifeMax = 10;
 
+	public int enemyLife = 10;
+	GameObject[] player = GameObject.FindGameObjectsWithTag("Player");
+	
 	// Use this for initialization
-	void Start () {
+	void Start () {	
 		patterns = GetComponent<EnemyPatterns>();
+		EBM = GetComponent<EnemyBulletManager>();
 	}
 	
 	// Update is called once per frame
@@ -25,9 +32,17 @@ public class BossEnemy : MonoBehaviour {
 			Destroy(this.gameObject);
 		}
 		
-		if(shotCtrl == 100){
-			//shot();
+		if(shotCtrl == 30){
 			shotCtrl = 0;
+			if(enemyLife < LifeMax * 0.3 ){
+				EBM.scatterShot((EnemyBulletManager.BulletColor)Random.Range(0,1), player[Random.Range(0,1)].transform.position, 6);
+			}else if(enemyLife < LifeMax * 0.5 ){
+			    EBM.circleShot((EnemyBulletManager.BulletColor)Random.Range(0,1), 36);
+			}else if(enemyLife < LifeMax * 0.8 ){
+			    EBM.WayShot((EnemyBulletManager.BulletColor)Random.Range(0,1), player[Random.Range(0,1)].transform.position, 3, 10);
+			}else{
+				EBM.aimShot((EnemyBulletManager.BulletColor)Random.Range(0,1), player[Random.Range(0,1)].transform.position);
+			}
 		}
 		
 		patterns.movePatterns(state);
@@ -39,11 +54,12 @@ public class BossEnemy : MonoBehaviour {
 			
 	}
 	
-	void OnTriggerEnter2D (Collider2D c){
+	void OnTriggerEnter2D (Collider2D bullet){
 		// delete bullet
-		Destroy(c.gameObject);
-
-		// life -1
-		enemyLife--;
+		if (bullet.gameObject.tag == "Player_BlueBullet" || bullet.gameObject.tag == "Player_RedBullet"){
+			Destroy(bullet.gameObject);
+			// life -1
+			enemyLife--;
+		}
 	}	
 }
